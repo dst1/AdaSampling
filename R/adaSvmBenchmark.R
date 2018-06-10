71c9ccc27c3f37be57449aa17a63d1782a8a5fb1
Pengyi Yang <yangpy7@gmail.com>
2018-06-10 18:23:35 +1000

    update

--- R/adaSvmBenchmark.R
+++ R/adaSvmBenchmark.R
@@ -51,7 +51,29 @@
 #' @param sampleFactor provides a control on the sample size for resampling.
 #' @return performance matrix
 #' @export
-
+#' @examples
+#' # Load the example dataset
+#' data(brca)
+#' head(brca)
+#' 
+#' # First, clean up the dataset to transform into the required format.
+#' brca.mat <- apply(X = brca[,-10], MARGIN = 2, FUN = as.numeric)
+#' brca.cls <- sapply(X = brca$cla, FUN = function(x) {ifelse(x == "malignant", 1, 0)})
+#' rownames(brca.mat) <- paste("p", 1:nrow(brca.mat), sep="_")
+#' 
+#' # Introduce 40% noise to positive class and 30% noise to the negative class
+#' set.seed(1)
+#' pos <- which(brca.cls == 1)
+#' neg <- which(brca.cls == 0)
+#' brca.cls.noisy <- brca.cls
+#' brca.cls.noisy[sample(pos, floor(length(pos) * 0.4))] <- 0
+#' brca.cls.noisy[sample(neg, floor(length(neg) * 0.3))] <- 1
+#' 
+#' # benchmark classification performance with different approaches
+#' \donttest{
+#' adaSvmBenchmark(data.mat = brca.mat, data.cls = brca.cls.noisy, data.cls.truth = brca.cls, cvSeed=1)
+#' }
+#' 
 adaSvmBenchmark <- function(data.mat, data.cls, data.cls.truth, cvSeed, C=50, sampleFactor=1){ 
   ##############------------Helper functions--------------##################################
   ### evaluation function
