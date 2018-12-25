fea78868e56093e7194e89066ef72623f4883f82
Unknown <doron.stupp@mail.huji.ac.il>
2018-12-25 20:40:28 +0200

    Documentation for AdaSample class
    Removed the original scikit template docstrings.

--- py/AdaSample.py
+++ py/AdaSample.py
@@ -5,27 +5,38 @@ from tqdm import tqdm
 
 
 class AdaSample(BaseEstimator):
-    """ A template estimator to be used as a reference implementation.
-    For more information regarding how to build your own estimator, read more
-    in the :ref:`User Guide <user_guide>`.
+    """
+    Basic scikit learn estimator wrapper for using AdaSampling
+    Based on the R package AdaSampling and the conference paper:
+    AdaSampling for positive-unlabeled and label noise learning with bioinformatics applications.
+    Yang, P., Ormerod, J., Liu, W., Ma, C., Zomaya, A., Yang, J.(2018) [doi:10.1109/TCYB.2018.2816984]
+
     Parameters
     ----------
-    demo_param : str, default='demo_param'
-        A parameter used for demonstation of how to pass and store paramters.
+    clf - base classifier to perform AdaSampling on. Should be a scikit-learn compatible classifier
+    with the methods - fit & predict_proba
     """
 
     def __init__(self, clf):
         self.base_estimator_ = clone(clf)
 
-    def fit(self, X, y, C=1, sampleFactor=1, seed=False, samp_frac=1, n_boost=25):
-        """A reference implementation of a fitting function.
+    def fit(self, X, y, C=1, sampleFactor=1, seed=False, n_rounds=25):
+        """
+        Fitting function
+        Adheres to arguments of the original R package.
+
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        X : numpy array, shape (n_samples, n_features)
             The training input samples.
         y : array-like, shape (n_samples,) or (n_samples, n_outputs)
-            The target values (class labels in classification, real numbers in
-            regression).
+            The target values - 1 for positive, 0 for negative.
+        C : number of ensemble classifiers (default: 1)
+        sampleFactor : float, subsampling factor (default: 1)
+        seed : bool, Whether to set seed
+               if True, seed is set to iteration index (default: False)
+        n_rounds: int, number of AdaSampling rounds (default: 25)
+
         Returns
         -------
         self : object
@@ -78,7 +89,30 @@ class AdaSample(BaseEstimator):
         return self
 
     def _fit_single(self, X, y, Ps, Ns, pos_probs, una_probs, sampleFactor, seed):
+        """
+        Helper function for fitting.
+        Similar to singleIter.R
 
+        Parameters
+        ----------
+        X : numpy array, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,)
+            The target values - 1 for positive, 0 for negative.
+        Ps : array-like, shape (n_positives,) indeces of positives
+        Ns : array-like, shape (n_negatives,) indeces of negatives
+        pos_probs : array-like, shape (n_positives,)
+                    probability of positives to belong to the positive class
+        una_probs : array-like, shape (n_negatives,)
+                    probability of unlabeled to belong to the negative class
+        sampleFactor : float, subsampling factor (default: 1)
+        seed : bool, Whether to set seed
+               if True, seed is set to iteration index (default: False)
+
+        Returns
+        -------
+        clf : fitted classifier object of type self.base_estimator_
+        """
         clf = clone(self.base_estimator_)
         if type(seed) == int:
             np.random.seed(seed)
@@ -100,15 +134,25 @@ class AdaSample(BaseEstimator):
         return clf
 
     def predict_proba(self, X, single=False):
-        """ A reference implementation of a predicting function.
+        """
+        Predicting function. predicts probailities for the ensemble.
+        Can be used in AdaSingle mode by setting single=True.
+        Then the function predicts based on the first classifier only.
+
+        When single = False, returns the average of predictions for the ensemble.
+
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        X : numpy array, shape (n_samples, n_features)
             The training input samples.
+        single : bool, whether to predict in mode AdaSingle (default: False)
+
         Returns
         -------
-        y : ndarray, shape (n_samples,)
-            Returns an array of ones.
+        y : ndarray, shape (n_samples,2)
+            Returns probabilities to belong to the negative class (col 0)
+            or the positive class (col 1)
+            Similar to other scikit-learn classifiers.
         """
         X = check_array(X, accept_sparse=True)
         check_is_fitted(self, 'is_fitted_')
